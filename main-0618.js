31a27b562dd2062c69aa83367b23917f789a8a82
wenpeng-w <449202631@qq.com>
2017-04-26 23:54:17 +0800

    修改个人主页

--- main-0618.js
+++
@@ -1,7 +0,0 @@
-(function(win){
-	var winW = document.documentElement.clientWidth;
-	var winH = document.documentElement.clientHeight;
-	var bannerWrap = document.getElementById("banner_wrap");
-	bannerWrap.style.width = winW + 'px';
-	bannerWrap.style.height = winH + 'px';
-})(window)
