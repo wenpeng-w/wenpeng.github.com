06ca07015770a61abe9879755115c740ebfcaa0b
wenpeng-w <449202631@qq.com>
2018-01-29 22:52:03 +0800

    2018/01/29

--- static/js/main.js
+++ static/js/main.js
@@ -8,4 +8,29 @@
     function random (lower, upper) {
         return Math.floor(Math.random() * (upper - lower)) + lower;
     }
+
+    // 手机端头部导航
+    $('#nav_btn').on('click', function () {
+        sliderToggle();
+    });
+    $('#nav_list').on('click', 'a', function () {
+        sliderToggle();
+    });
+    $('#nav_btn_close').on('click', function () {
+        sliderToggle();
+    });
+    var isShow = false;
+    function sliderToggle () {
+        $('#nav').fadeToggle(400);
+        if (isShow) {
+            $('#nav_list').animate({ 'right': '-70%' }, 300);
+            $('html body').css('overflow','auto');
+
+        } else {
+            $('#nav_list').animate({ 'right': '0' }, 300);
+            $('html body').css('overflow','hidden');
+        }
+        isShow = !isShow;
+    }
+
 })(jQuery);
