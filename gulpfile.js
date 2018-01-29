fa79d22c98621a1bfaaffc5d8687c15205d10d18
wenpeng-w <449202631@qq.com>
2018-01-29 16:07:42 +0800

    add tags page

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,6 @@ gulp.task('less', function () {
         .pipe(cssMinify({
             advanced: false, // 类型：Boolean 默认：true [是否开启高级优化（合并选择器等）]
             compatibility: 'ie8',
-            format: 'keep-breaks', // 类型：Boolean 默认：false [是否保留换行]
             keepSpecialComments: '*' // 保留所有特殊前缀 当你用autoprefixer生成的浏览器前缀，如果不加这个参数，有可能将会删除你的部分前缀
         }))
         .pipe(gulp.dest('static/css/'))
